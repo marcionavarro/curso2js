3faa31b58c96749ca78c6ad1bc2556dbf6f1f967
marcionavarro <marcionavarrodearaujo@gmail.com>
2022-05-19 01:00:13 -0300

    Correção upload Foto

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ class App {
   middelwares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, 'uploads')));
+    this.app.use(express.static(resolve(__dirname, '..', 'uploads', 'images')));
   }
 
   routes() {
